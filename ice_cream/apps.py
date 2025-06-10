7b84d4c83a1caf623d5640b2cf3f7d29e4396802
Lucas <lucas.exbrayat@gmail.com>
2025-06-10 20:52:52 +0200

    feat: add ice cream models (Flavor, Order) with stock management

---
+++ ice_cream/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class IceCreamConfig(AppConfig):
+    """
+    Configuration for the ice cream app
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ice_cream"
+    verbose_name = "Automate de Glaces"
